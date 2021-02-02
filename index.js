3997147e402a83b1b5720d43e97d506bd30a63eb
Nail-civan <borkaben@gmail.com>
2021-02-02 18:57:26 +0100

    f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("hi");
+
+const express = require('express')
+const app= express()
+
+const port = 3000
+
+app.get('/', function (req, res){
+
+    res.send('Hello There!!!!!')
+
+
+})
+
+app.listen(port, function(){
+
+    console.log(`lis ${port}`)
+
+
+})
